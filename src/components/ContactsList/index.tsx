f8acd35910cc6efc577e10938301b2f07870a1bf
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2022-07-04 23:00:36 -0300

    iniciando contacts list

--- src/components/ContactsList/index.tsx
+++ src/components/ContactsList/index.tsx
@@ -1,4 +1,9 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +12,35 @@ export default function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+      </ListContainer>
+
+      <Card>
+        <div className="info">
+          <div className="contact-name">
+            <strong>Lucas Korz</strong>
+            <small>instagram</small>
+          </div>
+          <span>lucaskorz10@gmail.com</span>
+          <span>(47) 98867-1028</span>
+        </div>
+
+        <div>
+          <a href="/">
+            <img src={edit} alt="edit" />
+          </a>
+          <button type="button">
+            <img src={trash} alt="edit" />
+          </button>
+        </div>
+      </Card>
     </Container>
   );
 }
--- src/components/ContactsList/styles.tsx
+++ src/components/ContactsList/styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   margin-top: 32px;
@@ -28,4 +28,32 @@ export const Header = styled.header`
       color: #fff;
     }
   }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-right: 8px;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.primary.main};
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0, 0.04);
+  padding: 16px;
+  border-radius: 4px;
 `;

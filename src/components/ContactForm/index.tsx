541efafd3afa0cd8bcf2fe770ac6e709f53f9c4c
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2022-08-03 21:40:36 -0300

    feat: changes ContactForm

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -3,31 +3,59 @@ import Input from "../Input";
 import Select from "../Select";
 import Button from "../Button";
 import { Form, ButtonContainer } from "./styles";
+import { FormEvent, useState } from "react";
 
 type ContactFormProps = {
   buttonLabel: string;
 }
 
 export default function ContactForm({ buttonLabel }: ContactFormProps) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [category, setCategory] = useState('')
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup
         error="O formato do e-mail é inválido"
       >
-        <Input placeholder="E-mail" error />
+        <Input
+          placeholder="E-mail"
+          error
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 

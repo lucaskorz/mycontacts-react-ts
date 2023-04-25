f8141b665e7d7ebd1996379d7858e5055281a6ee
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2023-04-24 22:16:29 -0300

    fix: adjust useHome

--- src/pages/Home/useHome.ts
+++ src/pages/Home/useHome.ts
@@ -34,6 +34,7 @@ export default function useHome() {
       setContacts(contactsList);
     } catch (error) {
       setHasError(true);
+      setContacts([])
     } finally {
       setIsLoading(false);
     }

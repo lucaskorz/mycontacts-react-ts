f1df16b3f86236f503605b2c51a526bc59aeb342
Lucas Korz <62682384+lucaskorz@users.noreply.github.com>
2023-04-24 22:14:08 -0300

    refactor: refactored conditional renders

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,12 +27,19 @@ export default function Home() {
     handleDeleteContact,
   } = useHome();
 
+  const hasContacts = contacts.length > 0;
+  const isListEmpty = !hasError && (!isLoading && contacts.length < 1);
+  const isSearchEmpty = !hasError && (hasContacts && filteredContacts.length < 1);
+
   return (
     <Container>
       <Loader isLoading={isLoading} />
 
-      {contacts.length > 0 && (
-        <InputSearch value={searchTerm} onChange={handleToggleSearchTerm} />
+      {hasContacts && (
+        <InputSearch
+          value={searchTerm}
+          onChange={handleToggleSearchTerm}
+        />
       )}
 
       <Header
@@ -42,15 +49,11 @@ export default function Home() {
       />
 
       {hasError && <ErrorStatus onTryAgain={handleTryAgain} />}
+      {isListEmpty && <EmptyList />}
+      {isSearchEmpty && <SearchNotFound searchTerm={searchTerm} />}
 
-      {!hasError && (
+      {hasContacts && (
         <>
-          {contacts.length < 1 && !isLoading && <EmptyList />}
-
-          {contacts.length > 0 && filteredContacts.length < 1 && (
-            <SearchNotFound searchTerm={searchTerm} />
-          )}
-
           <ContactsList
             filteredContacts={filteredContacts}
             orderBy={orderBy}
